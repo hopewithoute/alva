2e25cb44d437f61ae27a3b757547e2625d62228a
hopewithoute <anggi.wibiyanto@gmail.com>
2026-07-03 00:04:08 +0700

    fix: disable ssr on HelloWorld and add vue dir to tailwind source

--- alva_demo/lib/alva_demo_web/live/home_live.ex
+++ alva_demo/lib/alva_demo_web/live/home_live.ex
@@ -5,7 +5,7 @@ defmodule AlvaDemoWeb.HomeLive do
   def render(assigns) do
     ~H"""
     <div class="min-h-screen bg-gray-50 p-10">
-      <.vue v-component="HelloWorld" />
+      <.vue v-component="HelloWorld" v-ssr={false} />
     </div>
     """
   end
